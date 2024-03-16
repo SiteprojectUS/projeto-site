0e2b396ac73c0a2e19235fe89c49684b7663fdc6
SiteprojectUS <silvapazpaulohenrique@gmail.com>
2024-03-16 16:36:08 -0300

    modal

--- script.js
+++ script.js
@@ -201,3 +201,16 @@ function plusSlides(n) {
     }
     slides[slideIndex].style.display = "block";
 }
+
+//janela modal//
+function openModal(){
+    const modal = document.getElementById('modal-container')
+    modal.classList.add('mostrar')
+
+    modal.addEventListener('click', (e) =>{
+        if (e.target.id == 'modal-container' || e.target.id == "fechar"){
+            modal.classList.remove('mostrar')
+            localStorage.fechaModal = 'modal-container'
+        }
+    })
+}
